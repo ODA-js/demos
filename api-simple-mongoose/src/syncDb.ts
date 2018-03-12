a6ea8608b52b6491574bf0949b96992ee3a1eced
vedanta-krit.das <vedmalex@gmail.com>
2018-03-12 16:27:10 +0300

    mongoose demo

--- api-simple-mongoose/src/syncDb.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config({ silent: true });
-import { SystemGraphQL } from './model/runQuery';
-
-(async () => {
-  let connectors = await SystemGraphQL.connectors();
-  await connectors.syncDb(true);
-  await connectors.close();
-})();
