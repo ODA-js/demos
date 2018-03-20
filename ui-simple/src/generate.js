83f44f614c0c17de0d1cec6c9c714886eaab67a0
vedanta-krit.das <vedmalex@gmail.com>
2018-03-20 16:46:19 +0300

    ui

---
+++ ui-simple/src/generate.js
@@ -0,0 +1,23 @@
+var path = require('path');
+const { generator } = require('oda-gen-graphql');
+const schema = require('../../api-model-editor/dist/schema/index').default;
+const {
+  UserUI,
+  ToDoItemUI,
+} = require('./ui-hooks');
+
+generator({
+  hooks: [
+    UserUI,
+    ToDoItemUI
+  ],
+  pack: schema,
+  rootDir: path.join(__dirname, 'UI'),
+  config: {
+    ui: true,
+    ts: false,
+    graphql: false,
+    schema: false,
+    packages: ['system'],
+  },
+});
